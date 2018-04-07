0a916027607c22e00ab2d45b53848b47c428250a
Lorenz Walthert <lorenz.walthert@icloud.com>
2018-04-07 21:18:39 +0200

    regex according to @krlmlr, print stuff

--- tests/testthat/test-test.R
+++ tests/testthat/test-test.R
@@ -8,7 +8,14 @@ test_that("test with tempdir", {
   path_temp <- file.path(dir, "repeated_parsing-in.R")
   path_perm <- rprojroot::find_testthat_root_file("repeated_parsing-in.R")
   file.copy(path_perm, dir)
-  path_out <- gsub("\\-.*$", "\\-in\\.R", path_perm)
+  old_path_out <- gsub("\\-.*$", "\\-in\\.R", path_perm)
+  print("The old path is")
+  print(old_path_out)
+
+  path_out <- gsub("\\-[^-]*$", "\\-in\\.R", path_perm)
+  print("The new path is")
+  print(path_out)
+
   identical(path_out, path_perm)
   #> TRUE
   ref <- enc::read_lines_enc(path_out) # does not work with path_out, only works with path_term
